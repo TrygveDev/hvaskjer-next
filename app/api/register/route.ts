253263f80b76d79368e23d18418a5ff479b8baeb
Trygve <57544728+TrygveDev@users.noreply.github.com>
2023-04-25 21:25:03 +0200

    v1.1

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
 	const body = await request.json();
+	console.log(body);
 	const { email, name, password } = body;
 
 	const hashedPassword = await bcrypt.hash(password, 12);
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-import getCurrentUser from "./actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { useSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +15,6 @@ export default async function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	const currentUser = await getCurrentUser();
-
 	return (
 		<html lang="en">
 			<body className={inter.className}>
